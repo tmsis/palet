d8d7db33f4801ce1928387c00c95b7314768d863
jesse.beaumont@tensileai.com <jesse.beaumont@tensileai.com>
2021-12-16 14:33:39 -0500

    CC0 1.0 Universal

--- palet/create_monthly_var.py
+++
@@ -1,8 +0,0 @@
-month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-         'August', 'September', 'October', 'November', 'December', 'Latest']
-
-col = str(input())
-
-for val in month:
-    print("'" + val + " " + col + "'")
-# friendly_name = Med
