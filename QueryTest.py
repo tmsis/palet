d6df5a9a0513aa9d002a5ee2b884a2aa4264ffd2
Matt Fury <matthew.fury@cms.hhs.gov>
2022-01-18 16:49:39 -0500

    first pass at some fields and paletable import updates

--- QueryTest.py
+++ QueryTest.py
@@ -6,12 +6,13 @@ from palet.Trend import Trend
 from palet.Enrollment import Enrollment
 from palet.State import State
 from palet.Palet import Palet
+from palet.PaletMetadata import PaletMetadata
 
 State = State('NC')
 t = Trend().byMonth()
 # e = Enrollment(t).byAgeRange('0-18,65+').byState(State.state_fips).byIncomeBracket('01')
 # e = Enrollment(t).byIncomeBracket('01').byState(State.state_fips)
-e = Enrollment(t).byMonth().byState(State.state_fips)
+e = Enrollment(t).byMonth(PaletMetadata.Enrollment.CHIP.month.Dec).byState(State.state_fips)
 print(e.sql())
 e.fetch()
 
