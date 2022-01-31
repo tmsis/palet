04bc540d9204e22b62ffd81d3881fa85e4af17d0
Matt Fury <matthew.fury@cms.hhs.gov>
2022-01-31 16:28:38 -0500

    added argument based bat file. pass in version num

--- deploy-val.bat
+++ deploy-val.bat
@@ -1,3 +1,16 @@
+%echo off
+@REM Usage deploy-val.bat <version>
+@REM e.g. deploy-val.bat 1.2.1
+
+set ver=%1
+if not defined ver goto :usage
+if defined ver goto :proceed
+:usage
+%echo on
+Usage: deploy-val.bat <version number>
+exit /B 1
+:proceed
+%echo on
 rm -r -fo .\build; rm -r -fo .\*.egg-info
 python setup.py bdist_wheel
-databricks --profile val fs cp ./dist/palet-1.0.1-py3-none-any.whl dbfs:/FileStore/shared_uploads/akira/lib/palet-1.0.1-py3-none-any.whl --overwrite
+databricks --profile val fs cp ./dist/palet-1.0.1-py3-none-any.whl dbfs:/FileStore/shared_uploads/akira/lib/palet-%1-py3-none-any.whl --overwrite
