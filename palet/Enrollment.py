bf731513e49e2702c729635b0ce23576163a4d57
Charlie Hogan <charlie.hogan@tensileai.com>
2022-02-04 15:01:32 -0800

    Enrollment

--- palet/Enrollment.py
+++ palet/Enrollment.py
@@ -6,7 +6,6 @@ the Enrollment module inherits from the Paletable module.
 """
 
 import pandas as pd
-from build.lib import palet
 from palet.PaletMetadata import PaletMetadata
 from palet.Paletable import Paletable
 
