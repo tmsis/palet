2ef9f33f0d6f4d0a842d5040c13bcab36131a085
Charlie Hogan <charlie.hogan@tensileai.com>
2022-02-08 09:26:37 -0800

    Query test

--- QueryTest.py
+++ QueryTest.py
@@ -2,6 +2,7 @@
 # use it as you will.
 # TODO: Refactor to have automated testing for breakage
 
+from palet.Coverage import Coverage
 from palet.Eligibility import Eligibility
 from palet.Trend import Trend
 from palet.Enrollment import Enrollment
@@ -9,21 +10,24 @@ from palet.Enrollment import Enrollment
 from palet.Palet import Palet
 from palet.PaletMetadata import PaletMetadata
 
-print(PaletMetadata.Enrollment.raceEthnicity.race_ethncty_flag[1])
-# t = Trend().byMonth()
-# e = Enrollment().byAgeRange('0-18,65+')
-# e = Enrollment(t).byIncomeBracket('01').byState(State.state_fips)
-# e = Enrollment(t).byMonth(PaletMetadata.Enrollment.CHIP.monthly.Dec).byState(State.state_fips)
-# e = Enrollment().byGender('M').byRaceEthnicity('1')
-# e = Enrollment().byYear()
-e = Eligibility().byMonth()
-print(e.sql())
-e.fetch()
+# print(PaletMetadata.Enrollment.raceEthnicity.race_ethncty_flag[1])
+# # t = Trend().byMonth()
+# # e = Enrollment().byAgeRange('0-18,65+')
+# # e = Enrollment(t).byIncomeBracket('01').byState(State.state_fips)
+# # e = Enrollment(t).byMonth(PaletMetadata.Enrollment.CHIP.monthly.Dec).byState(State.state_fips)
+# # e = Enrollment().byGender('M').byRaceEthnicity('1')
+# # e = Enrollment().byYear()
+# e = Eligibility().byMonth()
+# print(e.sql())
+# e.fetch()
 
-Palet.Utils.propertiesOf(e)
+# Palet.Utils.propertiesOf(e)
 
-e = Enrollment()
-print(e.by_group)
+# e = Enrollment()
+# print(e.by_group)
 
 # print(StateHelper.displayValues())
 # .byState('37').byEthnicity('01').byAgeRange('18-21').byGender('F')
+
+api = Coverage().byType()
+api.sql()
