946ee53cd0cb54c2fa1549c3280ead1abfd23706
Matt Fury <matt.fury@tensileai.com>
2022-02-24 22:23:40 -0500

    linux version

---
+++ deploy-val.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+set echo off
+
+# Usage deploy-val.bat <version>
+# e.g. deploy-val.bat 1.2.1
+
+ver=$1
+if [ -z "$ver" ]
+then
+set echo on
+echo "Usage: deploy-val.sh <version number>"
+exit
+else
+set echo on
+echo "*********** cleaning up cache *************"
+rm -rf ./build; rm -rf /*.egg-info
+echo "*********** building new whl file *********"
+python ./setup.py bdist_wheel
+echo "*********** copying newly built wheel to databricks**********"
+databricks --profile val fs cp ./dist/palet-1.0.1-py3-none-any.whl dbfs:/FileStore/shared_uploads/akira/lib/palet-$1-py3-none-any.whl --overwrite
+
+fi
