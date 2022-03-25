078016660576ca58c1131656cc0ce61b072fcc6f
Charlie Hogan <charlie.hogan@tensileai.com>
2022-03-25 14:16:53 -0700

    Diagnoses.py documention

--- palet/Diagnoses.py
+++ palet/Diagnoses.py
@@ -1,3 +1,7 @@
+"""
+The Diagnoses module is a critical component of filtering :class:`Enrollment` by chronic coniditions. This module only contains the :class:`Diagnoses` class and the :meth:`Diagnoses.Diagnoses.where` static method.
+"""
+
 # -------------------------------------------------------
 #
 #
@@ -7,6 +11,11 @@ from palet.ServiceCategory import ServiceCategory
 
 
 class Diagnoses:
+    """
+    The Diagnoses class creates an alias called inpatient that transposes the 12 dgns_cd columns allowing :meth:`Enrollment.Enrollment.having` to filter
+    by various coverage types. It also plays a role in the backend method for decorating the chronic conidition column the user specifies.
+
+    """
 
     inpatient = ['dgns_1_cd',
                  'dgns_2_cd',
@@ -43,6 +52,39 @@ class Diagnoses:
     # -------------------------------------------------------
     @staticmethod
     def where(service_category: ServiceCategory, diagnoses: list):
+        """
+        The static method where() is used to assign parameters for the :meth:`~Enrollment.Enrollment.having` in :class:`Enrollment`. This is where the user assigns
+        a service category from the :class:`ServiceCategory` class and the list of diagnoses codes they have specified. 
+
+        Args:
+            service_category: `attribute`: Specify an attribute from the :class:`ServiceCategory` such as ServiceCategory.inpaitent
+            diagnoses: `list`: User defined list of diagnoses codes
+
+        Returns:
+            Spark DataFrame: returns the updated object
+
+        Note:
+            The where() function is nested within :meth:`~Enrollment.Enrollment.having`.
+
+        Example:
+            Create a list of diagnoses codes:
+
+            >>> AFib = ['I230', 'I231', 'I232', 'I233', 'I234', 'I235', 'I236', 'I237', 'I238', 'I213', 'I214', 'I219', 'I220', 'I221', 'I222', 'I228', 'I229', 'I21A1', 'I21A9', 'I2101', 'I2102', 'I2109', 'I2111', 'I2119', 'I2121', 'I2129']
+
+            Create an Enrollment Object:
+
+            >>> api = Enrollment.ByMonth()
+
+            Use the :meth:`~Enrollment.Enrollment.having` function with where() as a parameter to filter by chronic condition
+           
+            >>> df = api.having(Diagnoses.where(ServiceCategory.inpatient, AFib))
+
+            Return DataFrame
+
+            >>> display(df.fetch())
+
+        """
+        
         return f"""
             (
                 select distinct
