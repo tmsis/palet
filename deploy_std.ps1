044e2d9502c2dec40bf58dac97f034f4156f7c2f
Matt Fury <matthew.fury@cms.hhs.gov>
2022-04-01 10:47:45 -0400

    added the two additional cls

--- deploy_std.ps1
+++ deploy_std.ps1
@@ -5,5 +5,7 @@ databricks --profile val fs cp ./palet/Eligibility.py dbfs:/FileStore/shared_upl
 databricks --profile val fs cp ./palet/Paletable.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite  
 databricks --profile val fs cp ./palet/Coverage.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
 databricks --profile val fs cp ./palet/Palet.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
+databricks --profile val fs cp ./palet/Diagnoses.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
+databricks --profile val fs cp ./palet/ServiceCategory.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
 Write-Output "last updated on: " 
 Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
