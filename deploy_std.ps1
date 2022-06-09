71738366f4a4a671f5af007244369241f9ed435f
Charlie Hogan <charlie.hogan@tensileai.com>
2022-06-09 15:33:50 -0700

    Adding ClaimsAnalysis.py to std deployment file

--- deploy_std.ps1
+++ deploy_std.ps1
@@ -9,6 +9,8 @@ databricks --profile val fs cp ./palet/EnrollmentType.py dbfs:/FileStore/shared_
 databricks --profile val fs cp ./palet/ServiceCategory.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
 databricks --profile val fs cp ./palet/DateDimension.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
 databricks --profile val fs cp ./palet/Readmits.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
+databricks --profile val fs cp ./palet/ClaimsAnalysis.py dbfs:/FileStore/shared_uploads/akira/lib/palet/ --overwrite
+
 
 
 Write-Output "last updated on: " 
